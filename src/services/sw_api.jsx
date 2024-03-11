f283843748783e7c9695cdb6e4bbdd95b42eb8d8
Seny Irinarkhova <kseniiaIrinarkhova@gmail.com>
2024-03-11 14:40:42 -0700

    fetched starships data from api

--- src/services/sw_api.jsx
+++ src/services/sw_api.jsx
@@ -1,5 +1,12 @@
-async function getStarships(){
-    return "should return starships"
+import axios from 'axios';
+
+async function getStarships() {
+    //create simple url
+    const url = `${import.meta.env.VITE_API_URL}/starships/`;
+    //get first 10 starships
+    const response = await axios.get(url);
+    //return response data
+    return response.data
 }
 
-export {getStarships};
+export { getStarships };
